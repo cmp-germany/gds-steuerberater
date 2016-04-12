21fdaeda8fbe74fb5fdd02df8fcde5f11060377b
Kai Wittmann <kaisky89@gmail.com>
2016-04-12 20:19:51 +0200

    nahbar beim start nicht sichtbar

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
   // plugin is applied to a scrollable element, targeting my navigation element
   $('body').scrollspy({ 'target': '#bs-example-navbar-collapse-1' });
 
+  if ($('ul.nav li.active').hasClass('hidden')) {
+    $('.navbar').hide();
+  }
+
   // listen for scrollspy events on the navigation element itself
   $('#bs-example-navbar-collapse-1').on('activate.bs.scrollspy', function(e) {
     if ($(e.target).hasClass('hidden')){
