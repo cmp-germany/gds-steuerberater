7855eaf9e824744d2686ae125492480a16d4a630
kaisky89 <kaisky89@gmail.com>
2016-04-19 10:38:15 +0200

    Inhalte dynamisch machen: Kontakt

--- index.php
+++ index.php
@@ -20,7 +20,6 @@
       </button>
       <a class="navbar-brand page-scroll" href="#page-top">GDS <small>Steuerberatung</small></a>
     </div>
-
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <div class="navbar-left">
@@ -32,87 +31,30 @@
         <li class="hidden"><a class="page-scroll" href="#page-top"></a></li>
         <li class="nav-image-link"><a href="https://www.taxyourself.de" target="_blank"><img src="<?= get_template_directory_uri() ?>/gfx/menu-bar/tyl.gif" alt="Tax Yourself Logo" class="menu-bar-icon"/></a></li>
         <?php
-          if( have_rows('onepage_elemente') ):
-              while ( have_rows('onepage_elemente') ) : the_row();
-              if (get_sub_field('in_navigation_sichtbar')) {
+          while ( have_rows('onepage_elemente') ) : the_row();
+            if (get_sub_field('in_navigation_sichtbar')) {
+              if (get_sub_field('navigation_icon')) {
+                ?><li><a class="page-scroll" href="#<?= get_sub_field('navigation_link') ?>"><span class="glyphicon glyphicon-envelope"></span></a></li><?php
+              } else {
                 ?><li><a class="page-scroll" href="#<?= get_sub_field('navigation_link') ?>"><?= get_sub_field('navigation_beschriftung') ?></a></li><?php
               }
-              endwhile;
-
-          else :
-
-              // no layouts found
-
-          endif;
+            }
+          endwhile;
         ?>
-        <li><a class="page-scroll" href="#kontaktformular"><span class="glyphicon glyphicon-envelope"></span></a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
 
 <?php
-
-if( have_rows('onepage_elemente') ):
-  while ( have_rows('onepage_elemente') ) : the_row();
-    get_template_part('onepage', get_row_layout());
-  endwhile;
-else :
-    // no layouts found
-endif;
+  if( have_rows('onepage_elemente') ):
+    while ( have_rows('onepage_elemente') ) : the_row();
+      get_template_part('onepage', get_row_layout());
+    endwhile;
+  else :
+      // no layouts found
+  endif;
 ?>
-
-
-
-
-<section id="kontaktformular" class="kontakt-slide parallax-slide">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-10 col-md-offset-1 text-center">
-        <h1>Interesse?</h1>
-        <h2>Schreiben Sie uns, wir sind gerne für Sie da</h2>
-      </div>
-    </div>
-    <div class="kontakt-form">
-      <div class="row">
-        <?php
-          echo $response;
-        ?>
-        <form class="form-horizontal" action="<?php the_permalink(); ?>#kontaktformular" method="post">
-          <div class="form-group">
-            <label for="kontakt-name" class="col-sm-4 control-label">Ihr Vor- und Nachname</label>
-            <div class="col-sm-7">
-              <input type="text" class="form-control" id="kontakt-name" placeholder="Ihr Vor- und Nachname" name="message_name" value="<?php echo esc_attr($_POST['message_name']); ?>">
-            </div>
-          </div>
-          <div class="form-group">
-            <label for="kontakt-email" class="col-sm-4 control-label">Ihre Email Adresse</label>
-            <div class="col-sm-7">
-              <input type="email" class="form-control" id="kontakt-email" placeholder="Ihre Email Adresse" name="message_email" value="<?php echo esc_attr($_POST['message_email']); ?>">
-            </div>
-          </div>
-          <div class="form-group">
-            <label for="kontakt-nachricht" class="col-sm-4 control-label">Ihre Nachricht an uns</label>
-            <div class="col-sm-7">
-              <textarea class="form-control" id="kontakt-nachricht" rows="6" placeholder="Ihre Nachricht an uns" name="message_text"><?php echo esc_textarea($_POST['message_text']); ?></textarea>
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="col-sm-offset-4 col-sm-7">
-              <input type="hidden" name="mailto" value="kaisky89@gmail.com">
-              <input type="hidden" name="submitted" value="1">
-              <button type="submit" class="btn btn-default">Senden</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-</section>
-
-
-
-
 
 <?php endwhile; // end of the loop. ?>
 <?php get_footer(); ?>
---
+++ onepage-kontakt.php
@@ -0,0 +1,51 @@
+<?php global $response; ?>
+<section
+  <?php if (get_sub_field('in_navigation_sichtbar')){
+    echo "id=\"".get_sub_field('navigation_link'). "\"";
+  }; ?>
+  class="kontakt-slide parallax-slide">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-10 col-md-offset-1 text-center">
+        <h1><?= get_sub_field('titel') ?></h1>
+        <h2><?= get_sub_field('untertitel') ?></h2>
+      </div>
+    </div>
+    <div class="kontakt-form">
+      <div class="row">
+        <?php
+          echo $response;
+        ?>
+        <form class="form-horizontal" action="<?php the_permalink(); ?><?php if (get_sub_field('in_navigation_sichtbar')){
+          echo "#".get_sub_field('navigation_link');
+        }; ?>" method="post">
+          <div class="form-group">
+            <label for="kontakt-name" class="col-sm-4 control-label"><?= get_sub_field('beschriftung_name') ?></label>
+            <div class="col-sm-7">
+              <input type="text" class="form-control" id="kontakt-name" placeholder="<?= get_sub_field('beschriftung_name') ?>" name="message_name" value="<?php echo esc_attr($_POST['message_name']); ?>">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="kontakt-email" class="col-sm-4 control-label"><?= get_sub_field('beschriftung_email') ?></label>
+            <div class="col-sm-7">
+              <input type="email" class="form-control" id="kontakt-email" placeholder="<?= get_sub_field('beschriftung_email') ?>" name="message_email" value="<?php echo esc_attr($_POST['message_email']); ?>">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="kontakt-nachricht" class="col-sm-4 control-label"><?= get_sub_field('beschriftung_nachricht') ?></label>
+            <div class="col-sm-7">
+              <textarea class="form-control" id="kontakt-nachricht" rows="6" placeholder="<?= get_sub_field('beschriftung_nachricht') ?>" name="message_text"><?php echo esc_textarea($_POST['message_text']); ?></textarea>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-sm-offset-4 col-sm-7">
+              <input type="hidden" name="mailto" value="<?= get_sub_field('email_adresse_empfanger') ?>">
+              <input type="hidden" name="submitted" value="1">
+              <button type="submit" class="btn btn-default"><?= get_sub_field('beschriftung_button') ?></button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
