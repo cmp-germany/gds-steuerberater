6f8ac4673181030224d28ca657d491ad25451aba
kaisky89 <kaisky89@gmail.com>
2016-04-27 16:01:50 +0200

    act fields corrections

--- acf-fields.php
+++ acf-fields.php
@@ -956,7 +956,7 @@ acf_add_local_field_group(array (
 							'required' => 0,
 							'conditional_logic' => 0,
 							'wrapper' => array (
-								'width' => '',
+								'width' => 25,
 								'class' => '',
 								'id' => '',
 							),
